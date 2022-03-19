3f6fb06454efc4557f2651899d7964a5ee838663
Kayongo brian <kayonbrian@gmail.com>
2022-03-19 20:11:26 +0300

    all the crus and all the error handling has been handled

--- blog/main.py
+++ blog/main.py
@@ -21,6 +21,7 @@ def get_all_blogspot(db:Session = Depends(get_db)):
     return blogs
     
 
+
 @app.post('/blog/', status_code=status.HTTP_201_CREATED)
 def create(request:schemes.Blog, db:Session = Depends(get_db)):
     new_blog = models.Blog(title=request.title, body=request.body)  
@@ -38,11 +39,17 @@ def get_blog(id,response: Response, db:Session = Depends(get_db)):
         # return {"Datail":f"Blog with ID of {id} is not found"}
     return blog
 
+
 @app.delete('/blog/{id}', status_code=HTTPStatus.NO_CONTENT)
 def delete_blog(id,response: Response, db:Session = Depends(get_db)):
-    blog = db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"the blog {id} you want to update doesnot exist")
+    blog.delete(synchronize_session=False)    
     db.commit()
     return Response(status_code=HTTPStatus.NO_CONTENT.value)
+
+
 
 @app.put('/bog/{id}', status_code=status.HTTP_202_ACCEPTED)
 def blog_update(id,request:schemes.Blog, response: Response, db:Session = Depends(get_db)):
